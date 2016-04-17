901d314fcbafc148be2cfd12daa06460444657d2
EC2 Default User <ec2-user@ip-172-31-50-188.ec2.internal>
2016-04-17 17:07:54 +0000

    changes in application in cloud

--- app.js
+++ app.js
@@ -175,8 +175,6 @@ app.get('/logout', function (req, res) {
 });
 });
 
-app.listen(3000, function () {
-  console.log('Jammer listening on port 3000!');
+app.listen(80, function () {
+  console.log('Jammer listening on port: 80!');
 });
-
-//added comment
