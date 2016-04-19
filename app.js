0c0bae870f6ce770b0942c32379b10cc18452a43
EC2 Default User <ec2-user@ip-172-31-50-188.ec2.internal>
2016-04-19 23:24:39 +0000

    changed the text on statistics page to fit project name

--- app.js
+++ app.js
@@ -177,8 +177,8 @@ app.get('/logout', function (req, res) {
 });
 });
 
-app.listen(3000, function () {
-  console.log('Jammer listening on port 3000!');
+app.listen(80, function () {
+  console.log('Jammer listening on port 80!');
 });
 
 //added comment
